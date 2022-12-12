1cd29351f0d7f3476687f2620c1e516864c91fac
n1c0re <102040781+n1c0re@users.noreply.github.com>
2022-12-12 18:15:32 +0300

    +T-Rex

--- Bot.js
+++ Bot.js
@@ -1,7 +1,13 @@
 ﻿import axios from "axios";
 import { JSDOM } from "jsdom";
 import TelegramBot from "node-telegram-bot-api";
-// import fs from "fs";
+import { Express } from "express";
+import path from "path";
+
+const server = express();
+const port = process.env.PORT || 5000;
+const tRexGame = "T-Rex";
+const queries = {};
 
 // SYMBOL_X = '❌'
 // SYMBOL_O = '⭕'
@@ -10,13 +16,10 @@ const token = "5673090770:AAHZYfYHuCh8tzfshFf8Ro3jbKETTmdi9j8";
 
 const bot = new TelegramBot(token, { polling: true });
 
-// ссылка на игру в сети интернет
-let url = 'https://n1c0re.github.io/TicTacToe.html'
+const urlGame = "https://n1c0re.github.io/TicTacToe.html";
 
-// название игры (то, что указывали в BotFather)
-const gameName = "TicTacToe"
+const TicTacToeGame = "TicTacToe";
 
-//конфиг клавиатуры
 const keyboard = [
   [
     {
@@ -121,28 +124,62 @@ bot.on("callback_query", (query) => {
   const chatId = query.message.chat.id;
 
   if (query.data === "mem") {
-    const url = "https://www.memify.ru/";
-    let randomNumber;
+    // const urlMem = "https://www.memify.ru/";
 
-    axios.get(url).then((Response) => {
+    // let randomNumber;
+
+    // axios.get(urlMem).then((Response) => {
+    //   const currentPage = Response.data;
+
+    //   const dom = new JSDOM(currentPage);
+
+    //   const allElementsOfPage = dom.window.document.querySelectorAll(
+    //     ".infinite-item.card"
+    //   );
+
+    //   randomNumber = Math.round(Math.random() * allElementsOfPage.length);
+
+    //   console.log(allElementsOfPage.length);
+    //   console.log(randomNumber);
+    //   let imageText;
+
+    //   imageText = allElementsOfPage[randomNumber]
+    //     .querySelector(".card-text")
+    //     .textContent.trim();
+
+    //   let image = allElementsOfPage[randomNumber].querySelector("img").src;
+
+    //   bot.sendPhoto(chatId, image, { caption: imageText });
+    // });
+    let urlMeme = "https://www.memify.ru/meme/";
+
+    let randomNumber = Math.round(Math.random() * 90000);
+
+    urlMeme += randomNumber;
+
+    console.log(urlMeme);
+
+    axios.get(urlMeme).then((Response) => {
       const currentPage = Response.data;
 
       const dom = new JSDOM(currentPage);
 
       const allElementsOfPage =
-        dom.window.document.getElementsByClassName("infinite-item card");
+        dom.window.document.querySelectorAll(".card.meme-detail");
 
-      randomNumber = Math.floor(Math.random() * allElementsOfPage.length);
       console.log(allElementsOfPage.length);
-      console.log(randomNumber);
 
-      let imageText = allElementsOfPage[randomNumber]
-        .getElementsByClassName("card-text")[0]
-        .textContent.trim();
+      let imageText;
 
-      let image = allElementsOfPage[randomNumber].querySelector("img").src;
+      //console.log(allElementsOfPage[0]);
 
-      bot.sendPhoto(chatId, image, { caption: imageText });
+      imageText = allElementsOfPage[0].querySelector("div.card-text").text.trim();
+
+      console.log(imageText);
+        // .textContent.trim();
+
+      // let image = allElementsOfPage[randomNumber].querySelector("img").src;
+      // bot.sendPhoto(chatId, image, { caption: imageText });
     });
   }
 
@@ -152,7 +189,11 @@ bot.on("callback_query", (query) => {
     //    inline_keyboard: ticTacToe,
     //  },
     //});
-      bot.sendGame(chatId, gameName);
+    bot.sendGame(chatId, TicTacToeGame);
+  }
+
+  if (query.data === "tRex"){
+    bot.sendGame(chatId, tRexGame);
   }
 
   if (query.data === "ilya") {
@@ -160,6 +201,21 @@ bot.on("callback_query", (query) => {
   }
 });
 
-bot.on('callback_query', function onCallbackQuery(callbackQuery) {
-  bot.answerCallbackQuery(callbackQuery.id, { url });
+bot.on("callback_query", function onCallbackQuery(callbackQuery) {
+  bot.answerCallbackQuery(callbackQuery.id, { urlGame });
 });
+
+bot.on("callback_query", function (query) {
+  if (query.game_short_name !== gameName) {
+      bot.answerCallbackQuery(query.id, "Sorry, '" + query.game_short_name + "' is not available.");
+  } else {
+      queries[query.id] = query;
+      let gameurl = "https://trexgame.herokuapp.com/index.html?id="+query.id;
+      bot.answerCallbackQuery({
+          callback_query_id: query.id,
+          url: gameurl
+      });
+  }
+});
+
+server.use(express.static(path.join(__dirname, 'public')));
